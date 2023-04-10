d338b29796afdcd99b2e3faa5a2255c51ab0bc2a
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-10 11:12:35 -0300

    Setting tailwind project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',        
+        'white': '#ffffff',
+        'black': '#000000',
+        'mainColor': '#000F23',
+        'accentColor': '#E05414',
+      },
   },
   plugins: [],
 }
