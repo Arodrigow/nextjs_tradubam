4644635652b78a815254b8a38626856a976c2b69
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-10 11:14:33 -0300

    Styling ATF text

--- src/components/Abovethefold.tsx
+++ src/components/Abovethefold.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Abovethefold() {
   return (
-    <div>Abovethefold</div>
+  <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-image'>
+    <div className='absolute top-0 bottom-0 left-0 right-0 bg-mainColor/90 z-[2]'/>
+    <div className='p-5 z-[2]'>
+      <h2 className='pb-5 text-white text-5xl font-bold'>BAM!!</h2>
+      <hr className='w-2/12 border-2 ml-0 border-accentColor bg-accentColor'/>
+       <p className='py-5 text-lg text-white font-light'>Clientes novos ganham presentes por aqui! <br />
+        Por ser o seu primeiro trabalho conosco, ofertaremos um <br />
+       super desconto de 20%!</p> 
+      <button className='border border-transparent p-3 bg-accentColor rounded-full text-lg text-white font-bold'>Pedir orçamento</button>
+    </div>
+  </div>
   )
 }
 
