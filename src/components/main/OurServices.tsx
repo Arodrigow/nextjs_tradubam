45fbe6d8b248eb0e3e15e8d12909222a780c7419
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-11 16:08:53 -0300

    Started developing OurServices section of the main page

--- src/components/main/OurServices.tsx
+++ src/components/main/OurServices.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import LearnMoreButton from '../utils/LearnMoreButton'
 
 function OurServices() {
   return (
-    <div>OurServices</div>
+    <section className='flex justify-center items-center flex-col h-screen px-8'>
+      <h2 className='text-mainColor font-bold text-4xl py-12'>Nossos Serviços</h2>
+      <div className='block sm:grid sm:grid-cols-3 sm:gap-10'>
+          <div className='row-span-2 rounded-2xl p-4 bg-mainColor '> 
+            <h3 className='text-white text-3xl font-bold'>Precisa Que Traduzam O Seu Abstract?</h3>
+            <br />
+            <p className='text-white'>Quer publicar em uma revista internacional mas não sabe o idioma? <br/>
+                Precisa de ajuda para lidar com clientes estrangeiros?
+                <br/>
+                <br/>
+                Então, BAM! Os seus problemas estão revolvidos.</p>
+                <br />
+                <LearnMoreButton to="/"></LearnMoreButton>
+          </div>
+          <div> Teste 2</div>
+          <div> Teste 3</div>
+          <div> Teste 4</div>
+          <div> Teste 5</div>
+      </div>
+    </section>
   )
 }
 
