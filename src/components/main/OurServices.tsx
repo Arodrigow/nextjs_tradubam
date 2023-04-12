91a13f1d7aae569e6a8ab4836e58a1a7413b8bda
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-12 14:43:51 -0300

    Finalizing the first draft of styling of the section, implemented MainServiceCards

--- src/components/main/OurServices.tsx
+++ src/components/main/OurServices.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import {AiFillBook, AiFillRead} from 'react-icons/ai'
+import {BsPcDisplay} from 'react-icons/bs'
+import {FaBusinessTime} from 'react-icons/fa'
 import LearnMoreButton from '../utils/LearnMoreButton'
+import MainServiceCard from '../utils/MainServiceCard'
 
 function OurServices() {
+  const services = [
+    {
+      serviceName: "Tradução",
+      serviceDescription: "Oferecemos serviços de tradução Simples e Técnica.",
+      serviceIcon: <AiFillBook className='fill-accentColor w-6 h-auto'/>,     
+    },
+    {
+      serviceName: "Versão",
+      serviceDescription: "Oferecemos serviços de versão Simples e Técnica.",
+      serviceIcon: <AiFillRead className='fill-accentColor w-6 h-auto'/>, 
+    },
+    {
+      serviceName: "Audiovisual",
+      serviceDescription: "Podemos ajudar em seu projeto audiovisual através dos serviços de Transcrição e Legendagem.",
+      serviceIcon: <BsPcDisplay className='fill-accentColor w-6 h-auto'/>,
+    },
+    {
+      serviceName: "Para empresas",
+      serviceDescription: "Aqui na BAM te damos o suporte necessário para que a sua empresa avance sem os impedimentos da língua estrangeira.",
+      serviceIcon: <FaBusinessTime className='fill-accentColor w-6 h-auto'/>,
+    },    
+  ]
+
   return (
-    <section className='flex justify-center items-center flex-col h-screen px-8'>
+    <section className='flex justify-center items-center flex-col p-8 '>
       <h2 className='text-mainColor font-bold text-4xl py-12'>Nossos Serviços</h2>
       <div className='block sm:grid sm:grid-cols-3 sm:gap-10'>
-          <div className='row-span-2 rounded-2xl p-4 bg-mainColor '> 
+          <div className='hidden sm:block sm:row-span-2 sm:rounded-2xl sm:p-4 sm:bg-mainColor '> 
             <h3 className='text-white text-3xl font-bold'>Precisa Que Traduzam O Seu Abstract?</h3>
             <br />
             <p className='text-white'>Quer publicar em uma revista internacional mas não sabe o idioma? <br/>
@@ -17,11 +44,12 @@ function OurServices() {
                 <br />
                 <LearnMoreButton to="/"></LearnMoreButton>
           </div>
-          <div> Teste 2</div>
-          <div> Teste 3</div>
-          <div> Teste 4</div>
-          <div> Teste 5</div>
-      </div>
+        {
+        services.map(function({serviceName, serviceDescription, serviceIcon}, index) {
+          return <MainServiceCard key={index} serviceName={serviceName} serviceDescription={serviceDescription} serviceIcon={serviceIcon}></MainServiceCard>
+        })
+        }
+      </div> 
     </section>
   )
 }
