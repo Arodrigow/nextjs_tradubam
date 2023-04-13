053d31aa852fba70d5d048267959f807a49cf61d
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-13 09:46:33 -0300

    Added data and images to the clients used in the avaliation section of the main page

--- src/components/main/OurClients.tsx
+++ src/components/main/OurClients.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Slider from '../utils/Slider'
 
 function OurClients() {
   return (
-    <div>OurClients</div>
+    <section className='flex justify-center items-center flex-col px-8 py-8  mb-12 bg-white'>
+    <h2 className='text-mainColor font-bold text-4xl py-6'>Nossos Clientes Satisfeitos!</h2>
+    <hr className='w-2/6 sm:w-1/12 border-2 mb-10 border-accentColor bg-accentColor'/>
+    <Slider></Slider>
+  </section>
   )
 }
 
