5cd2d5b22b75ae533b199931c922c7806fb039b7
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-13 15:29:28 -0300

    Created and added a footer to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
+import Footer from '@/components/utils/Footer'
 import NavBar from '@/components/utils/NavBar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <><NavBar></NavBar><Component {...pageProps} /></>
+    <><NavBar></NavBar>
+    <Component {...pageProps} />
+    <Footer></Footer></>
   )
-    
+
 }
