0e7d5cc4a99f30e5b9cac5f3872f9f404a324dec
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-13 15:15:18 -0300

    Client testimony card added and styled

---
+++ src/components/utils/ClientAvaliation.tsx
@@ -0,0 +1,32 @@
+import Image, { StaticImageData } from 'next/image';
+import { BsChatLeftQuoteFill } from 'react-icons/bs'
+import React from 'react'
+
+interface props {
+    client: {
+        name: string,
+        position: string;
+        text: string;
+        image: StaticImageData;
+    }
+}
+
+function ClientAvaliation({ client }: props) {
+    return (
+        <div className='flex flex-col justify-between bg-mainColor rounded-2xl p-4 sm:p-12 h-[500px] sm:h-[400px] sm:w-[80%] text-sm text-center sm:text-base'>
+            <BsChatLeftQuoteFill className='fill-accentColor w-[40px] h-auto sm:w-[32px] sm:h-[32px]'></BsChatLeftQuoteFill>
+            <br />
+            <p className='text-white'>{client.text}</p>
+            <br />
+            <div className='flex justify-end'>
+                <Image src={client.image} alt={'Image of our happy client'} className='w-[70px] h-auto row-span-2 rounded-full'></Image>
+                <div className='flex flex-col justify-center pl-4'>
+                    <p className='text-white'>{client.name}</p>
+                    <p className='text-accentColor'>{client.position}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ClientAvaliation
