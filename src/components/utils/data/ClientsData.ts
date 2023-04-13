28b7b590101ff3d1e4576aa1929e76da6c65a339
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-13 10:26:23 -0300

    Added clients dataset

---
+++ src/components/utils/data/ClientsData.ts
@@ -0,0 +1,24 @@
+import ImageClient2 from './../../../public/images/ImageClient2.jpeg'
+import ImageClient1 from './../../../public/images/ImageClient1.jpeg'
+import ImageClient3 from './../../../public/images/ImageClient3.jpeg'
+
+export const Clients = [
+    {
+        name: "Rodrigo Sobral",
+        position:"Web Designer",
+        text:"Os tradutores da BAM trabalharam comigo durante quatro anos em um dos meus sites, desempenhando funções  envolvendo tradução de  conteúdo e legendagem. Eles são muito responsáveis e organizados, sempre finalizando os projetos com excelência. Recomendo a empresa e voltaria a trabalhar com ela.",
+        image:  ImageClient1
+    },
+    {
+        name: "Maria Paula Santos",
+        position:"Engenheira Hídrica",
+        text:"Rápida e cirúrgica! Com traduções de qualidade ímpar e revisões que trazem muito mais acurácia e linguagem técnica condizente à minha área de atuação (Engenharia Hídrica e Microbiologia Ambiental), a BAM é minha principal escolha para a tradução/revisão de artigos para publicações internacionais e para trabalhos voltados a públicos muito mais críticos e exigentes na área de pesquisas acadêmicas.",
+        image:  ImageClient2
+    },
+    {
+        name: "Maria Paula Santos",
+        position:"Arquiteta",
+        text:"Os tradutores da BAM me ajudaram nas traduções dos artigos científicos que publicamos no grupo do mestrado da UFVJM. E também na tradução do minha dissertação final do mestrado. Trabalho excelente, com qualidade e prazo impecável. Recomendo muito!!!",
+        image:  ImageClient3
+    },
+]
