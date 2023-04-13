0dea12bc05df7e3297109f5fcc4a7abac241cb16
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-13 16:35:20 -0300

    Styled the footer component

--- src/components/utils/Footer.tsx
+++ src/components/utils/Footer.tsx
@@ -1,8 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='block md:flex md:justify-around sm:items-center w-full h-[75px] md:h-[50px] text-mainColor font-semibold md:font-bold bg-accentColor'>
+      <div className=' flex justify-around items-center'>
+        <ul className='px-0 sm:px-4'><Link href={'/'}>Início</Link></ul>
+        <ul className='px-0 sm:px-4'><Link href={'/'}>Empresa</Link></ul>
+        <ul className='px-0 sm:px-4'><Link href={'/'}>Serviços</Link></ul>
+        <ul className='px-0 sm:px-4'><Link href={'/'}>Contato</Link></ul>
+      </div>
+      <div className='text-sm flex flex-col text-center pt-2 md:pt-0 md:flex-row'>
+      <p >Copyright © 2023 BAM</p>
+      <p className='hidden md:block md:px-4'> | </p>
+      <p>Created by <a href='https://andremaciel.dev.br/' target="_blank" rel="noopener noreferrer">André Maciel</a> </p>
+      </div>
+        
+    </div>
   )
 }
 
