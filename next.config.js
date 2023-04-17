dbbef9dfee6f394c925eb3e3df4e35fec6eebdd8
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-17 14:40:55 -0300

    Added list of locales

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n:{
+    locales:['en-us', 'pt-br'],
+    defaultLocale: 'pt-br'
+  }
 }
 
 module.exports = nextConfig
