a4186eaca6f4bbd6ed1d614ac89ead65decaebf6
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-18 10:38:37 -0300

    Added countries data and flags - used in localization

---
+++ src/components/utils/data/Flags.ts
@@ -0,0 +1,13 @@
+import Portuguese from './../../../public/images/Portuguese.png'
+import English from './../../../public/images/English.png'
+
+export const Flags = [
+    {
+        name: 'Português',
+        source: Portuguese
+    },
+    {
+        name: 'English',
+        source: English
+    }
+]
