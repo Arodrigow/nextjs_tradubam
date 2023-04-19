135185c57e33173202c6e3c9ec1d4b1dedbeaf34
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-19 14:27:56 -0300

    Added localization to OurServices component

--- src/components/main/OurServices.tsx
+++ src/components/main/OurServices.tsx
@@ -4,46 +4,50 @@ import {BsPcDisplay} from 'react-icons/bs'
 import {FaBusinessTime} from 'react-icons/fa'
 import LearnMoreButton from '../utils/LearnMoreButton'
 import MainServiceCard from '../utils/MainServiceCard'
+import { useTranslation } from 'next-i18next'
 
 function OurServices() {
+
+  const { t:translation } = useTranslation(['index'])
+
   const services = [
     {
-      serviceName: "Tradução",
-      serviceDescription: "Oferecemos serviços de tradução Simples e Técnica.",
+      serviceName: translation("Tradução"),
+      serviceDescription: translation("Oferecemos serviços de tradução Simples e Técnica."),
       serviceIcon: <AiFillBook className='fill-accentColor w-6 h-auto'/>,     
     },
     {
-      serviceName: "Versão",
-      serviceDescription: "Oferecemos serviços de versão Simples e Técnica.",
+      serviceName: translation("Versão"),
+      serviceDescription: translation("Oferecemos serviços de versão Simples e Técnica."),
       serviceIcon: <AiFillRead className='fill-accentColor w-6 h-auto'/>, 
     },
     {
-      serviceName: "Audiovisual",
-      serviceDescription: "Podemos ajudar em seu projeto audiovisual através dos serviços de Transcrição e Legendagem.",
+      serviceName: translation("Audiovisual"),
+      serviceDescription: translation("Podemos ajudar em seu projeto audiovisual através dos serviços de Transcrição e Legendagem."),
       serviceIcon: <BsPcDisplay className='fill-accentColor w-6 h-auto'/>,
     },
     {
-      serviceName: "Para empresas",
-      serviceDescription: "Aqui na BAM te damos o suporte necessário para que a sua empresa avance sem os impedimentos da língua estrangeira.",
+      serviceName: translation("Para empresas"),
+      serviceDescription: translation("Aqui na BAM te damos o suporte necessário para que a sua empresa avance sem os impedimentos da língua estrangeira."),
       serviceIcon: <FaBusinessTime className='fill-accentColor w-6 h-auto'/>,
     },    
   ]
 
   return (
     <section className='flex justify-center items-center flex-col px-8 py-4  mb-12'>
-      <h2 className='text-center text-mainColor font-bold text-4xl py-6'>Nossos Serviços</h2>
+      <h2 className='text-center text-mainColor font-bold text-4xl py-6'>{translation("Nossos Serviços")}</h2>
       <hr className='w-2/6 sm:w-1/12 border-2 mb-10 border-accentColor bg-accentColor'/>
       <div className='block sm:grid sm:grid-cols-3 sm:gap-10'>
           <div className='hidden sm:block sm:row-span-2 sm:rounded-2xl sm:p-4 sm:bg-mainColor sm:shadow-lg sm:shadow-mainColor'> 
-            <h3 className='text-white text-3xl font-bold'>Precisa Que Traduzam O Seu Abstract?</h3>
+            <h3 className='text-white text-3xl font-bold'>{translation("Precisa Que Traduzam O Seu Abstract?")}</h3>
             <br />
-            <p className='text-white'>Quer publicar em uma revista internacional mas não sabe o idioma? <br/>
-                Precisa de ajuda para lidar com clientes estrangeiros?
+            <p className='text-white'>{translation("Quer publicar em uma revista internacional mas não sabe o idioma?")} <br/>
+                {translation("Precisa de ajuda para lidar com clientes estrangeiros?")}
                 <br/>
                 <br/>
-                Então, BAM! Os seus problemas estão revolvidos.</p>
+                {translation("Então, BAM! Os seus problemas estão revolvidos.")}</p>
                 <br />
-                <LearnMoreButton to="/"></LearnMoreButton>
+                <LearnMoreButton to="/" text={translation("Saber mais.")}></LearnMoreButton>
           </div>
         {
         services.map(function({serviceName, serviceDescription, serviceIcon}, index) {
--- src/components/utils/LearnMoreButton.tsx
+++ src/components/utils/LearnMoreButton.tsx
@@ -2,14 +2,15 @@ import Link from 'next/link'
 import React from 'react'
 
 interface props{
-  to: string
+  to: string,
+  text:string
 }
 
-function LearnMoreButton({to}:props) {
+function LearnMoreButton({to, text}:props) {
 
   return (
     <Link href={to}>
-         <button className='border border-transparent py-2 px-5 bg-accentColor rounded-full text-base text-white font-bold'>Saiba mais</button>
+         <button className='border border-transparent py-2 px-5 bg-accentColor rounded-full text-base text-white font-bold'>{text}</button>
     </Link>
     
   )
