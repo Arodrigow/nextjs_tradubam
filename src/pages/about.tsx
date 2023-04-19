03d266dc0a3869e517b59d1dcc7d4399a147ef88
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-19 16:35:05 -0300

    Started adding content (already localized, pt/en) to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,24 +1,26 @@
+import Title from '@/components/about/Title'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
 
-function about() {
-  return (
-    <div>about</div>
-  )
+interface props {
+    locale: string
 }
 
-interface props {
-  locale:string
+function about() {
+    return (
+        <Title></Title>
+    )
 }
-export async function getStaticProps({locale}:props) {
+
+export async function getStaticProps({ locale }: props) {
     return {
-      props: {
-        ...(await serverSideTranslations(locale, [
-          'common',
-          'index',
-          'all'
-        ])),
-      },
+        props: {
+            ...(await serverSideTranslations(locale, [
+                'common',
+                'about',
+                'all'
+            ])),
+        },
     }
-  }
+}
 export default about
