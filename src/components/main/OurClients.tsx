d1a71fe745c1e99c9ef80732a2e720a0c8ec5a00
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-19 15:03:22 -0300

    Added localization (pt, en) to OurClients component

--- src/components/main/OurClients.tsx
+++ src/components/main/OurClients.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Slider from '../utils/Slider'
+import { useTranslation } from 'next-i18next'
 
 function OurClients() {
+  const { t:translation } = useTranslation(['index'])
   return (
     <section className='flex justify-center items-center flex-col sm:px-8 sm:py-8  mb-12 bg-white'>
-    <h2 className='text-center text-mainColor font-bold text-4xl py-6'>Nossos Clientes Satisfeitos!</h2>
+    <h2 className='text-center text-mainColor font-bold text-4xl py-6'>{translation("Nossos Clientes Satisfeitos!")}</h2>
     <hr className='w-2/6 sm:w-1/12 border-2 mb-10 border-accentColor bg-accentColor'/>
     <Slider></Slider>
   </section>
--- src/components/utils/ClientAvaliation.tsx
+++ src/components/utils/ClientAvaliation.tsx
@@ -1,6 +1,7 @@
 import Image, { StaticImageData } from 'next/image';
 import { BsChatLeftQuoteFill } from 'react-icons/bs'
 import React from 'react'
+import { useTranslation } from 'next-i18next';
 
 interface props {
     client: {
@@ -12,17 +13,18 @@ interface props {
 }
 
 function ClientAvaliation({ client }: props) {
+    const { t:translation } = useTranslation(['index'])
     return (
         <div className='flex flex-col justify-between bg-mainColor rounded-2xl p-4 sm:p-12 h-[500px] sm:h-[400px] sm:w-[80%] text-sm text-center sm:text-base sm:shadow-lg sm:shadow-black'>
             <BsChatLeftQuoteFill className='fill-accentColor w-[40px] h-auto sm:w-[32px] sm:h-[32px]'></BsChatLeftQuoteFill>
             <br />
-            <p className='text-white'>{client.text}</p>
+            <p className='text-white'>{translation(client.text)}</p>
             <br />
             <div className='flex justify-center sm:justify-end w-full'>
                 <Image src={client.image} alt={'Image of our happy client'} className='w-[70px] h-auto row-span-2 rounded-full'></Image>
                 <div className='flex flex-col justify-center pl-4'>
                     <p className='text-white'>{client.name}</p>
-                    <p className='text-accentColor'>{client.position}</p>
+                    <p className='text-accentColor'>{translation(client.position)}</p>
                 </div>
             </div>
         </div>
