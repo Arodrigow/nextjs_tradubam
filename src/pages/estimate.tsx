4ecd923cc84b297b9fc977a6ac189dfd6fb53642
André Rodrigo B A Maciel <rodrigow_@hotmail.com.br>
2023-04-24 16:34:21 -0300

    Added estimate page and localization files, also localized the basic title text

---
+++ src/pages/estimate.tsx
@@ -0,0 +1,28 @@
+import Title from '@/components/utils/Title'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import React from 'react'
+
+function estimate() {
+    const {t:translation} = useTranslation('estimate')
+  return (
+    <>
+    <Title title={translation("Orçamento")} subtitle={translation("Peça grátis!")}></Title>
+    </>
+  )
+}
+interface props {
+    locale:string
+  }
+export async function getStaticProps({locale}:props) {
+    return {
+      props: {
+        ...(await serverSideTranslations(locale, [
+          'common',
+          'estimate',
+          'all'
+        ])),
+      },
+    }
+  }
+export default estimate
